dc22a2bb4aaceeb205cef412e67e1214cfbd0638
megatzafran <2021854946@student.uitm.edu.my>
2022-02-20 13:10:57 +0800

    purging unwanted modules

---
+++ CSC Project/Aircraft.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define fiveTabs "\n\n\n\n\n"
+#define fourTabs "\n\n\n\n"
+
+int main()
+{
+
+    printf("\n\n%s   -------Collections of Aircraft------\n\n", fiveTabs);
+
+    // AIRCRAFT 1
+    printf("%s Boeing 777\n\n", fiveTabs);
+    printf("%s______                                                    \n", fiveTabs);
+    printf("%s         _\\ _~-\\___                                       \n", fourTabs);
+    printf("%s =  = ==(____AA____D                                      \n", fourTabs);
+    printf("%s             \\_____\\___________________,-~~~~~~~`-.._     \n", fourTabs);
+    printf("%s             /     o O o o o o O O o o o o o o O o  |\\_   \n", fourTabs);
+    printf("%s             `~-.__        ___..----..                  ) \n", fourTabs);
+    printf("%s                   `---~~\\___________/------------`````   \n", fourTabs);
+    printf("%s                   =  ===(_________D                      \n", fourTabs);
+
+    // AIRCRAFT 2
+    printf("\n\n%sJunkers Ju-87D Stuka\n", fiveTabs);
+    printf("%s------\n", fourTabs);
+    printf("%s | | # \\                                      |  \n", fourTabs);
+    printf("%s | ____ \\_________|----^\"|\"\"\"\"\"|\"\\___________ |  \n", fourTabs);
+    printf("%s  \\___\\   FO + 94 >>    `\"\"\"\"\"\"\"\"     =====  \"|D \n", fourTabs);
+    printf("%s        ^^-------____--\"\"\"\"\"\"\"\"\"\"+\"\"--_  __--\"|  \n", fourTabs);
+    printf("%s                    `\"\"|\"-->####)+---|`\"\"     |  \n", fourTabs);
+    printf("%s                                  \\  \\           \n", fourTabs);
+    printf("%s                                 <- O -)         \n", fourTabs);
+    printf("%s                                   `\"'           \n", fourTabs);
+
+    // AIRCRAFT 3
+    printf("\n%sF - 16\n", fiveTabs);
+    printf("%s  ___                                      \n", fourTabs);
+    printf("%s |   \\                                     \n", fourTabs);
+    printf("%s |    \\                   ___              \n", fourTabs);
+    printf("%s |_____\\______________.-'`   `'-.,___      \n", fourTabs);
+    printf("%s/| _____     _________            ___>---  \n", fourTabs);
+    printf("%s\\|___________________________,.-'`         \n", fourTabs);
+    printf("%s          `'-.,__________)                 \n", fourTabs);
+
+    // AIRCRAFT 4
+    printf("\n\n%sConcorde\n", fiveTabs);
+    printf("%s              .------,                      \n", fourTabs);
+    printf("%s              =\\      \\                     \n", fourTabs);
+    printf("%s .---.         =\\      \\                    \n", fourTabs);
+    printf("%s | C~ \\         =\\      \\                   \n", fourTabs);
+    printf("%s |     `----------'------'----------,       \n", fourTabs);
+    printf("%s.'     LI.-.LI LI LI LI LI LI LI.-.LI`-.    \n", fourTabs);
+    printf("%s\\ _/.____|_|______.------,______|_|_____)   \n", fourTabs);
+    printf("%s                 /      /                   \n", fourTabs);
+    printf("%s               =/      /                    \n", fourTabs);
+    printf("%s              =/      /                     \n", fourTabs);
+    printf("%s             =/      /                      \n", fourTabs);
+    printf("%s             /_____,'                       \n", fourTabs);
+}
--- CSC Project/Prototype.c
+++ CSC Project/Prototype.c
@@ -8,7 +8,6 @@
 
 int Booking();
 void Destination(void);
-void Aircraft(void);
 int billBooking(char destinationCharacter, char name[50], char nationality[50], int age, int numberTicket);
 
 int main()
@@ -48,9 +47,6 @@ int main()
             break;
         case 1:
             Destination();
-            break;
-        case 2:
-            Aircraft();
             break;
         default:
             printf("\n\n%s          Invalid input", sixTabs);
@@ -203,57 +199,4 @@ void Destination(void)
     printf("%s       [D]                Johor Bharu           10:00am           11:15pm          RM185.50 \n", twoTabs);
     printf("%s       [E]                Penang                11:00am           12:30pm          RM190.50 \n", twoTabs);
     printf("%s       [F]                Alor Setar            12:00am           2:00pm           RM210.00 \n", twoTabs);
-}
-
-void Aircraft(void)
-{
-    printf("\n\n%s   -------Collections of Aircraft------\n\n", fiveTabs);
-
-    // AIRCRAFT 1
-    printf("%s Boeing 777\n\n", fiveTabs);
-    printf("%s______                                                    \n", fiveTabs);
-    printf("%s         _\\ _~-\\___                                       \n", fourTabs);
-    printf("%s =  = ==(____AA____D                                      \n", fourTabs);
-    printf("%s             \\_____\\___________________,-~~~~~~~`-.._     \n", fourTabs);
-    printf("%s             /     o O o o o o O O o o o o o o O o  |\\_   \n", fourTabs);
-    printf("%s             `~-.__        ___..----..                  ) \n", fourTabs);
-    printf("%s                   `---~~\\___________/------------`````   \n", fourTabs);
-    printf("%s                   =  ===(_________D                      \n", fourTabs);
-
-    // AIRCRAFT 2
-    printf("\n\n%sJunkers Ju-87D Stuka\n", fiveTabs);
-    printf("%s------\n", fourTabs);
-    printf("%s | | # \\                                      |  \n", fourTabs);
-    printf("%s | ____ \\_________|----^\"|\"\"\"\"\"|\"\\___________ |  \n", fourTabs);
-    printf("%s  \\___\\   FO + 94 >>    `\"\"\"\"\"\"\"\"     =====  \"|D \n", fourTabs);
-    printf("%s        ^^-------____--\"\"\"\"\"\"\"\"\"\"+\"\"--_  __--\"|  \n", fourTabs);
-    printf("%s                    `\"\"|\"-->####)+---|`\"\"     |  \n", fourTabs);
-    printf("%s                                  \\  \\           \n", fourTabs);
-    printf("%s                                 <- O -)         \n", fourTabs);
-    printf("%s                                   `\"'           \n", fourTabs);
-
-    // AIRCRAFT 3
-    printf("\n%sF - 16\n", fiveTabs);
-    printf("%s  ___                                      \n", fourTabs);
-    printf("%s |   \\                                     \n", fourTabs);
-    printf("%s |    \\                   ___              \n", fourTabs);
-    printf("%s |_____\\______________.-'`   `'-.,___      \n", fourTabs);
-    printf("%s/| _____     _________            ___>---  \n", fourTabs);
-    printf("%s\\|___________________________,.-'`         \n", fourTabs);
-    printf("%s          `'-.,__________)                 \n", fourTabs);
-
-    // AIRCRAFT 4
-    printf("\n\n%sConcorde\n", fiveTabs);
-    printf("%s              .------,                      \n", fourTabs);
-    printf("%s              =\\      \\                     \n", fourTabs);
-    printf("%s .---.         =\\      \\                    \n", fourTabs);
-    printf("%s | C~ \\         =\\      \\                   \n", fourTabs);
-    printf("%s |     `----------'------'----------,       \n", fourTabs);
-    printf("%s.'     LI.-.LI LI LI LI LI LI LI.-.LI`-.    \n", fourTabs);
-    printf("%s\\ _/.____|_|______.------,______|_|_____)   \n", fourTabs);
-    printf("%s                 /      /                   \n", fourTabs);
-    printf("%s               =/      /                    \n", fourTabs);
-    printf("%s              =/      /                     \n", fourTabs);
-    printf("%s             =/      /                      \n", fourTabs);
-    printf("%s             /_____,'                       \n", fourTabs);
 }

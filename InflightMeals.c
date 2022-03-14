8a9d5e8050b492b8d697840a4b71907f83e91a9d
megatzafran <2021854946@student.uitm.edu.my>
2022-03-14 20:51:21 +0800

    pass

---
+++ InflightMeals.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    char meal, repeat;
+    float price, total = 0;
+
+    printf("---MEAL---\n");
+    printf("[A] Nasi lemak\t\t(RM15.00)\n");
+    printf("[B] Nasi kandar\t\t(RM15.00)\n");
+    printf("[C] Sandwich\t\t(RM 7.00)\n");
+    printf("[D] Nasi goreng ayam\t(RM15.00)\n");
+    printf("[E] Mee kari\t\t(RM10.00)\n");
+    printf("[F] Spaghetti bolognese\t(RM13.00)\n");
+
+    do
+    {
+        printf("\nEnter code of meal: ");
+        scanf(" %c", &meal);
+        printf("Total: ");
+        scanf("%d", &num);
+
+        switch (meal)
+        {
+        case 'a':
+        case 'A':
+            price = num * 15.00;
+            break;
+        case 'b':
+        case 'B':
+            price = num * 15.00;
+            break;
+        case 'c':
+        case 'C':
+            price = num * 7.00;
+            break;
+        case 'd':
+        case 'D':
+            price = num * 15.00;
+            break;
+        case 'e':
+        case 'E':
+            price = num * 10.00;
+            break;
+        case 'f':
+        case 'F':
+            price = num * 13.00;
+            break;
+        default:
+            printf("Please enter right code of meal\n");
+            break;
+        }
+
+        printf("Do you want to book another meal (Y/N): ");
+        scanf(" %c", &repeat);
+
+        total += price;
+    } while (repeat == 'Y' || repeat == 'y');
+
+    printf("\nTotal price: %.2f", total);
+    return 0;
+}
